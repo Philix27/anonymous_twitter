25342111780eb19a0fec601e92023cd33aece3ec
philix27 <philixbob@gmail.com>
2023-06-18 23:13:37 +0100

    removed unused packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import { type AppType } from "next/app";
 import "../src/styles/index.scss";
-// import Layout from "@/app/layout";
 import { MasaProvider } from "@masa-finance/masa-react";
 import { WagmiConfig, createConfig, mainnet } from "wagmi";
 import { createPublicClient, http } from "viem";
+import LayoutComp from "@/src/comp/global/Layout";
 
 const config = createConfig({
   autoConnect: true,
@@ -16,19 +16,9 @@ const config = createConfig({
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <WagmiConfig config={config}>
-      {/* <MasaProvider
-        company="Masa"
-        walletsToUse={["metamask", "valora"]}
-        forceNetwork={"alfajores"}
-        useRainbowKitWalletConnect
-        // forceNetwork="celo"
-        rainbowKitModalSize={"wide"}
-        verbose={false}
-      > */}
-      {/* <Layout> */}
-      <Component {...pageProps} />
-      {/* </Layout> */}
-      {/* </MasaProvider> */}
+      <LayoutComp>
+        <Component {...pageProps} />
+      </LayoutComp>
     </WagmiConfig>
   );
 };
--- pages/wagmi.tsx
+++ pages/wagmi.tsx
@@ -1,9 +1,24 @@
+import { AppMasa } from "@/src/lib";
 import React from "react";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsName,
+  useEnsAvatar,
+  useBalance,
+  useQuery,
+} from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 
 export default function Home() {
   const { address, isConnected } = useAccount();
+  const { data } = useBalance({ address: address });
+  const { data: ensData } = useEnsName({ address: address });
+
+  AppMasa.account.getBalances(address).then((e) => {
+    console.log("AppMasa", e);
+  });
 
   const { connect } = useConnect({
     connector: new InjectedConnector(),
@@ -13,8 +28,11 @@ export default function Home() {
   if (isConnected)
     return (
       <div>
-        Connected to {address}
+        <p>Connected to {address}</p>
+        <p>Balance to {data?.formatted}</p>
+        <p>Ens: {ensData?.length}</p>
         <button onClick={() => disconnect()}>Disconnect</button>
+        {/* <p>BB: {bb}</p> */}
       </div>
     );
   return <button onClick={() => connect()}>Connect Wallet</button>;
